b4986bc02d6e01a7ac4a4fb6c1570d64004eb357
Luis Valgoi <luisvalgoi@gmail.com>
2023-02-06 17:35:04 -0300

    refactor: adjusts click

--- src/components/SidebarControls/index.tsx
+++ src/components/SidebarControls/index.tsx
@@ -5,22 +5,37 @@ import HamburgerIcon from '@/assets/icons/hamburger.svg'
 
 import styles from './index.module.scss'
 
-const SidebarControls: React.FC<{} & JSX.IntrinsicElements['li']> = (props) => {
+const SidebarControls: React.FC<{} & JSX.IntrinsicElements['aside']> = (
+    props
+) => {
     const [collapsed, setCollapsed] = useState(false)
 
-    const sidebarClasses = classNames(styles.sidebarControls)
+    const sidebarClasses = classNames(styles.sidebarControls, {
+        [styles.sidebarCollapsed]: collapsed,
+    })
 
-    const headingClasses = classNames(styles.heading)
+    const headClasses = classNames(styles.sidebarControlsHead, {
+        [styles.sidebarControlsHeadCollapsed]: collapsed,
+    })
+
+    const toggleClasses = classNames(styles.sidebarControlsToggle)
+
+    const childrenClasses = classNames(styles.sidebarControlsChildren, {
+        [styles.sidebarControlsChildrenCollapsed]: collapsed,
+    })
 
     return (
         <aside className={sidebarClasses} {...props}>
-            <div className={styles.sidebarControlsHead}>
-                <h1 className={headingClasses}>Library</h1>
-                <button onClick={() => setCollapsed(!collapsed)}>
+            <head className={headClasses}>
+                <h1>Library</h1>
+                <button
+                    className={toggleClasses}
+                    onClick={() => setCollapsed(!collapsed)}
+                >
                     <img src={HamburgerIcon} alt="menu" />
                 </button>
-            </div>
-            {props.children}
+            </head>
+            <div className={childrenClasses}>{props.children}</div>
         </aside>
     )
 }
